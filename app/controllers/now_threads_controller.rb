99e89004ce4a840b353d385d2617a0a2c3522666
never_anarchy <neveranarchy@gmail.com>
2020-01-28 02:27:50 +0900

    fix now threads

--- app/controllers/now_threads_controller.rb
+++ app/controllers/now_threads_controller.rb
@@ -1,9 +1,9 @@
 class NowThreadsController < ApplicationController
   def index
-    a = Itest5ch::Board.find('liveanarchy').threads.partition do |thread|
+    arr = Itest5ch::Board.find('liveanarchy').threads do |thread|
       thread.dat.to_s.start_with?('924')
     end
-    @threads924 = a[0]
-    @threads = a[1]
+    @threads924 = arr[0]
+    @threads = arr[1]
   end
 end
