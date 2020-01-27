d56dd71460372bee387cbb690e256c81d0487791
never_anarchy <neveranarchy@gmail.com>
2020-01-28 03:13:40 +0900

    revert

--- app/controllers/now_threads_controller.rb
+++ app/controllers/now_threads_controller.rb
@@ -1,6 +1,6 @@
 class NowThreadsController < ApplicationController
   def index
-    arr = Itest5ch::Board.find('liveanarchy').threads do |thread|
+    arr = Itest5ch::Board.find('liveanarchy').threads.partition do |thread|
       thread.dat.to_s.start_with?('924')
     end
     @threads924 = arr[0]
